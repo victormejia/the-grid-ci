61c1fd0d23e0ed8b34f3af2cdf4abcd3ef661fbb
Victor Mejia <victormejia@users.noreply.github.com>
2018-04-10 10:29:22 -0700

    Revert ip (#8)
    * revert ip column

--- src/app/core/helpers.spec.ts
+++ src/app/core/helpers.spec.ts
@@ -8,8 +8,7 @@ export const mockHackers = [
     cityStateZip: 'Cartwrightview, South Carolina 24722',
     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/ashocka18/128.jpg',
     phone: '(775) 232-7260',
-    statusMessage:
-      'Use the optical RAM pixel, then you can navigate the online protocol!',
+    statusMessage: 'Use the optical RAM pixel, then you can navigate the online protocol!',
     specialty: 'bypassing pixel',
     ip: '187.154.44.205',
     email: 'Price.Donnelly9_Thompson37@gmail.com',
@@ -22,11 +21,9 @@ export const mockHackers = [
     dob: '1972-08-23T15:22:34.694Z',
     address: '7557 Flatley Well',
     cityStateZip: 'Lebsackport, Minnesota 33620-5654',
-    avatar:
-      'https://s3.amazonaws.com/uifaces/faces/twitter/oktayelipek/128.jpg',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/oktayelipek/128.jpg',
     phone: '(309) 006-4485',
-    statusMessage:
-      "You can't quantify the interface without programming the neural COM matrix!",
+    statusMessage: "You can't quantify the interface without programming the neural COM matrix!",
     specialty: 'copying array',
     ip: '194.74.47.30',
     email: 'Monica_Kub27@hotmail.com',
@@ -38,11 +35,9 @@ export const mockHackers = [
     dob: '1980-10-04T21:35:51.869Z',
     address: '975 Jayme Pass',
     cityStateZip: 'West Lizeth, Michigan 98486',
-    avatar:
-      'https://s3.amazonaws.com/uifaces/faces/twitter/travis_arnold/128.jpg',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/travis_arnold/128.jpg',
     phone: '(217) 365-6272',
-    statusMessage:
-      "I'll transmit the online HDD transmitter, that should bus the XML driver!",
+    statusMessage: "I'll transmit the online HDD transmitter, that should bus the XML driver!",
     specialty: 'quantifying sensor',
     ip: '73.226.119.184',
     email: 'Gregg_Bode2157@yahoo.com',
@@ -57,12 +52,12 @@ export const mockHackers = [
     cityStateZip: 'Sophiaton, Nevada 85879-3435',
     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/mslarkina/128.jpg',
     phone: '(851) 416-3782',
-    statusMessage:
-      "You can't program the bus without bypassing the redundant RSS circuit!",
+    statusMessage: "You can't program the bus without bypassing the redundant RSS circuit!",
     specialty: 'generating protocol',
     ip: '198.18.111.138',
     email: 'Kathlyn_Predovic_Cole@yahoo.com',
-    password: 'VV4YWbZTjbt0b9c'
+    password: 'VV4YWbZTjbt0b9c',
+    status: 'safe'
   },
   {
     id: 'dede14f9-dedb-4862-857d-f3c2015dcd56',
@@ -70,11 +65,9 @@ export const mockHackers = [
     dob: '1953-03-26T00:12:42.110Z',
     address: '087 Ariane Park',
     cityStateZip: 'Emardborough, Maine 30850',
-    avatar:
-      'https://s3.amazonaws.com/uifaces/faces/twitter/stefanozoffoli/128.jpg',
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/stefanozoffoli/128.jpg',
     phone: '(674) 331-5468',
-    statusMessage:
-      "You can't back up the firewall without navigating the optical EXE bus!",
+    statusMessage: "You can't back up the firewall without navigating the optical EXE bus!",
     specialty: 'hacking pixel',
     ip: '234.199.57.0',
     email: 'Ricky.Kohler_Gleichner@yahoo.com',
